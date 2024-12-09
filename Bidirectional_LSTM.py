b1fb081898c06a64bd802f59a34b63c3c8e5e6cb
gokul <gokulgsj2003@gmail.com>
2024-12-09 17:12:07 +0000

    Third commit

---
+++ Bidirectional_LSTM.py
@@ -0,0 +1,103 @@
+
+
+# import
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+
+#set device
+device = "cuda"
+
+#hyper parameters
+input_size = 784
+sequence_length = 28
+num_layers = 2
+hidden_size = 256
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epoch = 1
+
+class BRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(BRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_size*2, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device)
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+
+        return out
+
+#load data
+train_dataset = datasets.MNIST(root='dataset/', train = True, transform=transforms.ToTensor(), download=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_dataset = datasets.MNIST(root='dataset/', train = False, transform=transforms.ToTensor(), download=True)
+test_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+#init network
+model = BRNN(input_size=input_size,hidden_size=hidden_size, num_classes=num_classes, num_layers=num_layers).to(device)
+
+#loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+#train
+for epoch in range(num_epoch):
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        #get data to cuda
+        data = data.view(data.size(0), sequence_length, input_size).to(device)
+        targets = targets.to(device=device)
+
+        #forward
+        scores = model(data)
+        loss = criterion(scores, targets)
+
+        #backwards
+        optimizer.zero_grad()
+        loss.backward()
+
+        #gradient descent or adam step
+        optimizer.step()
+
+#check accuracy on training
+
+def check_accuracy(loader, model):
+    if loader.dataset.train:
+        print('checking accuracy on training data')
+    else:
+        print('checcking accuracy on test data')
+
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x,y in loader:
+            x=x.to(device=device)
+            y = y.to(device=device)
+            x = x.reshape(x.shape[0], -1)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions ==y).sum()
+            num_samples += predictions.size(0)
+
+        print(f'got {num_correct} / {num_samples} with accuracy {float(num_correct) / float(num_samples) * 100:.2f}%')
+
+    model.train()
+
+check_accuracy(train_loader, model)
+check_accuracy(test_loader,model)
+
