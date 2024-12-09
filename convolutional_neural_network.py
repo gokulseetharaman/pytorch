cc12990e4db9a153556383dbdb17ce0ac70a18fd
gokul <gokulgsj2003@gmail.com>
2024-12-09 15:52:55 +0000

    Second commit

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -7,27 +7,35 @@ from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
-#create fully connected network
-class NN(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(NN, self).__init__()
-        self.fc1 = nn.Linear(input_size, 50)
-        self.fc2 = nn.Linear(50, num_classes)
+#create simple CNN
+class CNN(nn.Module):
+    def __init__(self, in_channel = 1, num_classes = 10):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.conv2 = nn.Conv2d(in_channels = 8, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1)) # default stride=(1,1), padding=(1,1)
+        self.fc1 = nn.Linear(16*7*7, num_classes)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
         return x
+
 
 #set device
 device = "cuda"
 
 #hyper parameters
-input_size = 784
+input_size = 1
 num_classes = 10
 learning_rate = 0.001
 batch_size = 64
-num_epoch = 1
+num_epoch = 5
 
 #load data
 train_dataset = datasets.MNIST(root='dataset/', train = True, transform=transforms.ToTensor(), download=True)
@@ -36,7 +44,7 @@ test_dataset = datasets.MNIST(root='dataset/', train = False, transform=transfor
 test_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
 #init network
-model = NN(input_size=input_size, num_classes=num_classes).to(device)
+model = CNN().to(device)
 
 #loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -48,9 +56,6 @@ for epoch in range(num_epoch):
         #get data to cuda
         data = data.to(device=device)
         targets=targets.to(device=device)
-
-        #get the correct shape
-        data = data.reshape(data.shape[0], -1 )
 
         #forward
         scores = model(data)
@@ -79,7 +84,6 @@ def check_accuracy(loader, model):
         for x,y in loader:
             x=x.to(device=device)
             y = y.to(device=device)
-            x = x.reshape(x.shape[0], -1)
 
             scores = model(x)
             _, predictions = scores.max(1)
