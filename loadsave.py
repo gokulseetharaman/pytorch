4231730dd0a71a857b6d944c31be28797d420737
gokul <gokulgsj2003@gmail.com>
2024-12-13 17:09:51 +0000

    save and load the model

--- loadsave.py
+++ loadsave.py
@@ -10,6 +10,33 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import sys
 
+
+class CNN(nn.Module):
+    def __init__(self, in_channel = 1, num_classes = 10):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.conv2 = nn.Conv2d(in_channels = 8, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1)) # default stride=(1,1), padding=(1,1)
+        self.fc1 = nn.Linear(16*7*7, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
+        return x
+def save_checkpoint(state, filename = "my_checkpoint.pth.tar"):
+    print('saving checkpoint')
+    torch.save(state, filename)
+
+def load_checkpoint(checkpoint):
+    print('Loading checkpoint')
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
 #set device
 device = "cuda"
 
@@ -28,16 +55,17 @@ test_dataset = datasets.MNIST(root='dataset/', train = False, transform=transfor
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
 #init network
-model = torchvision.models.vgg16(pretrained = True)
-print(model)
-sys.exit()
+model = CNN(in_channel=in_channels, num_classes=num_classes).to(device)
+# print(model)
+# sys.exit()
 
 #loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 if load_model:
-    load_chekpoint(torch.load("my_checkpoint.pth.tar"))
+    load_checkpoint(torch.load("my_checkpoint.pth.tar"))
+
 
 #train
 for epoch in range(num_epoch):
